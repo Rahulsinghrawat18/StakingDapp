df20f02e9aaef95a16a4f76e7d1ec069dad94155
Rahulsinghrawat18 <rawatji7788@gmail.com>
2025-05-01 00:50:02 +0530

    Implementing StakingDapp.sol

--- contracts/StakingDapp.sol
+++ contracts/StakingDapp.sol
@@ -39,17 +39,101 @@ contract StakingDapp is Ownable, ReentrancyGuard {
 
     //Contract Functions
 
-    function addPool() {}
+    function addPool(IERC20 _depositToken, IERC20 _rewardToken, uint256 _apy, uint _lockDays) public onlyOwner {
 
-    function deposit() {}
+        poolInfo.push(PoolInfo({
+            depositToken: _depositToken,
+            rewardToken: _rewardToken,
+            depositedAmount: 0,
+            apy: _apy,
+            lockDays: _lockDays,
+        }));
 
-    function withdraw() {}
+        poolCount++;
+    }
 
-    function _calcPendingReward() {}
+    function deposit(uint _pid, uint _amount) public nonReentrant {
+        require(_amount >0 , "Amount should be greater than 0");
 
-    function pendingReward() {}
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo string user = userInfo[_pid][msg.sender];
 
-    function sweep() {}
+        if(user.amount > 0){
+            uint pending = _calcPendingReward(user, _pid);
+            pool.rewardToken.transfer(msg.sender, pending);
+
+            _createNotification(_pid, pending, msg.sender, "Claim");
+        }
+
+        pool.depositToken.transferFrom(msg.sender, address(this), _amount);
+
+        pool.depositedAmount += _amount;
+
+        user.amount += _amount;
+        user.lastRewardAt = block.timestamp;
+
+        // user.lockUntil = block.timestamp + (pool.lockDays * 86400)
+        user.lockUntil = block.timestamp + (pool.lockDays * 60)
+
+        depositedTokens[address(pool.depositToken)] += _amount;
+
+        _createNotification(_pid, _amount, msg.sender, "Deposit");
+    }
+
+    function withdraw(uint _pid, uint _amount) public nonReentrant {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo string user = userInfo[_pid][msg.sender];
+
+        require(user.amount >= _amount , "Withdraw amount exceed the balance");
+        require(user.lockUntil <= block.timestamp, "Lock is Active!!");
+
+        uint256 pending = _calcPendingReward(user, _pid);
+        if(user.amount > 0) {
+            pool.rewardToken.transfer(msg.sender, pending);
+
+            _createNotification(_pid, pending, msg.sender, "Claim");
+        }
+
+        if(_amount > 0){
+            user.amount -= _amount;
+            pool.depositedAmount -= _amount;
+            depositedTokens[address(pool.depositToken)] -= _amount;
+
+            pool.depositToken.transfer(msg.sender, _amount);
+        }
+
+        user.lastRewardAt = block.timestamp;
+
+        _createNotification(_pid, _amount, msg.sender, "Withdraw");   
+    }
+
+    function _calcPendingReward(UserInfo storage user, uint _pid) internal view returns {
+        PoolInfo storage pool = poolInfo[_pid];
+
+        // uint daysPassed = (block.timestamp - user.lastRewardAt) /84600;
+
+        uint daysPassed = (block.timestamp - user.lastRewardAt) /60;
+
+        if(daysPassed > pool.lockDays) {
+            daysPassed = pool.lockDays;
+        }
+
+        return user.amount * daysPassed / 365 / 100 * pool.apy;
+    }
+
+    function pendingReward(uint _pid, address _user) public view returns(uint) {
+        UserInfo string user = userInfo[_pid][_user];
+        return _calcPendingReward(user, _pid);
+    }
+
+    function sweep(address token, uint256 amount) external onlyOwner {
+        uint256 token_balance = IERC20(token).balanceOf(address(this));
+
+        require(amount <= token_balance, "Amount exceeds balance");
+        require(token_balance - amount >= depositedTokens[token], "Can't withdraw deposited tokens");
+
+        IERC20(token).safeTransfer(msg.sender, amount);
+    }
 
     function modifyPool() {}
 
